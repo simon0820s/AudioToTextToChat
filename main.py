8d039800aa52b319b12bc0b4b486f27fe525e11c
simon0820s <simon0820s@gmail.com>
2023-11-04 22:37:09 -0500

    use gpt

--- main.py
+++ main.py
@@ -1,16 +1,33 @@
 import speech_recognition as sr
+import openai
 
-def useRecognizer():
+def useModels(audio):
+  text = useRecognizer(audio)
+  print(text)
+  response = useGPT(text)
+  print(response)
+  
+
+def useRecognizer(audio):
 
   r = sr.Recognizer()
-  audio = sr.AudioFile('luisAudio.wav')
 
   with audio as source:
     audio_to_analyze = r.record(source)
 
   text = r.recognize_google(audio_to_analyze, language='es')
 
-  print(text)
+  return text
+
+def useGPT(text):
+  text = "que piensas acerca de lo siguiente: " + text + "?"
+  response = openai.ChatCompletion.create(
+    model="gpt-3.5-turbo",
+    prompt=(text)
+
+)
+  return response
 
 if __name__=='__main__':
-  useRecognizer()
+  audio = sr.AudioFile('luisAudio.wav')
+  useModels(audio)
