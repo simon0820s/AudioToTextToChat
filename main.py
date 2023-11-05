ee85a4b50a2db0a83f920579c1096b703053c755
simon0820s <simon0820s@gmail.com>
2023-11-04 20:59:00 -0500

    get text of audio

--- main.py
+++ main.py
@@ -1,1 +1,16 @@
-import openai
+import speech_recognition as sr
+
+def useRecognizer():
+
+  r = sr.Recognizer()
+  audio = sr.AudioFile('luisAudio.wav')
+
+  with audio as source:
+    audio_to_analyze = r.record(source)
+
+  text = r.recognize_google(audio_to_analyze, language='es')
+
+  print(text)
+
+if __name__=='__main__':
+  useRecognizer()
